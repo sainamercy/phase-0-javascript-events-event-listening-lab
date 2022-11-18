b9b5552bd0261a2aceecaefde40d897530fd0949
sainamercy <sainamercy@gmail.com>
2022-11-18 08:43:30 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const btn = document.getElementById('button');
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
 function addingEventListener() {
+    btn.addEventListener("click", clickAlert);
 }
+
+addingEventListener();
